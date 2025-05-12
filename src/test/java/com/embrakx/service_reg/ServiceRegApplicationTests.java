5af20ef0ff2ece733b9c858bd95610a8915dfa63
Harshal <farkadeharshal14@gmail.com>
2025-05-12 11:07:04 +0530

    initinal commit

---
+++ src/test/java/com/embrakx/service_reg/ServiceRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embrakx.service_reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
